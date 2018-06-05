576ee360ad58cf1a579515429a8d1d4c1a31ea23
Leo Young <420760135@qq.com>
2018-06-05 22:58:15 +0800

    refactor: use Generators

--- sort/sort.js
+++ sort/sort.js
@@ -33,30 +33,24 @@ function genRenderList(min, max, size) {
   return list;
 }
 
-Array.prototype.swap = async function (i, j) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      var tmp = this[j];
-      this[j] = this[i];
-      this[i] = tmp;
-      drawBarChart(this);
-      resolve();
-    }, 1000);
-  });
+Array.prototype.swap = function (i, j) {
+  var tmp = this[j];
+  this[j] = this[i];
+  this[i] = tmp;
 }
 
-async function bubbleSort(data) {
+function * bubbleSort(data) {
   for (var i = 0; i < data.length; i++) {
     for (var j = i + 1; j < data.length; j++) {
       if (data[j] < data[i]) {
-        await data.swap(i, j);
+        data.swap(i, j);
+        yield data;
       }
     }
   }
-  return data;
 }
 
-async function selectSort(data) {
+function* selectSort(data) {
   for (var i = 0; i < data.length; i++) {
     var minIndex = i;
     for (var j = i + 1; j < data.length; j++) {
@@ -65,23 +59,23 @@ async function selectSort(data) {
       }
     }
     if (minIndex !== i) {
-      await data.swap(minIndex, i);
+      data.swap(minIndex, i);
+      yield data;
     }
   }
-  return data;
 }
 
-async function insertSort(data) {
+function* insertSort(data) {
   for (var i = 0; i < data.length; i++) {
     for (var j = i; j > 0 && data[j] < data[j - 1]; j--) {
-      await data.swap(j, j - 1);
+      data.swap(j, j - 1);
+      yield data;
     }
   }
-  return data;
 }
 
-async function quickSort(data) {
-  async function sort(l, u) {
+function* quickSort(data) {
+  function* sort(l, u) {
     if (l >= u) {
       return;
     }
@@ -89,34 +83,27 @@ async function quickSort(data) {
     for (var i = l + 1; i <= u; i++) {
       if (data[i] < data[l]) {
         m++;
-        await data.swap(m, i);
+        data.swap(m, i);
+        yield data;
       }
     }
-    await data.swap(l, m);
-    await sort(l, m - 1);
-    await sort(m + 1, u);
+    data.swap(l, m);
+    yield data;
+    yield* sort(l, m - 1);
+    yield* sort(m + 1, u);
   }
-  await sort(0, data.length - 1);
-  return data;
+  yield* sort(0, data.length - 1);
 }
 
-async function runSortCode(sort, data) {
-  window
-    .performance
-    .clearMarks();
-  window
-    .performance
-    .clearMeasures();
-  window
-    .performance
-    .mark('sort_begin');
-  data = await sort(data);
-  window
-    .performance
-    .mark('sort_end');
-  window
-    .performance
-    .measure('sort', 'sort_begin', 'sort_end');
-  console.table(window.performance.getEntriesByType('measure'));
-  drawBarChart(data);
+function runSortCode(sort, data, timeout) {
+  var iter = sort(data);
+  var run = function () {
+    drawBarChart(data);
+    var currentData = iter.next();
+    if (!currentData.done) {
+      data = currentData.value;
+      setTimeout(run, timeout);
+    }
+  };
+  setTimeout(run, timeout);
 }
