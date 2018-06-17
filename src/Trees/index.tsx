d5dba4c7cce25a6c9bb1bc17d8fef6e50ab2eabd
Leo Young <420760135@qq.com>
2018-06-17 17:52:31 +0800

    feat: draw tree edges

--- src/Trees/index.tsx
+++ src/Trees/index.tsx
@@ -14,72 +14,96 @@ const treeSearchAlgs = {
 interface ITree {
   value: number;
   children: Array<ITree> | null;
+  x: number;
+  y: number;
+  r: number;
+  color: string;
+  bgColor: string;
 }
 
 const nodesData: ITree = {
   value: 0,
+  x: 0,
+  y: 0,
+  r: 15,
+  color: 'white',
+  bgColor: 'green',
   children: [{
     value: 1,
+    x: 0,
+    y: 0,
+    r: 15,
+    color: 'white',
+    bgColor: 'red',
     children: [{
       value: 3,
+      x: 0,
+      y: 0,
+      r: 15,
+      color: 'white',
+      bgColor: 'red',
       children: null
     }, {
       value: 4,
+      x: 0,
+      y: 0,
+      r: 15,
+      color: 'white',
+      bgColor: 'red',
       children: null
     }]
   }, {
     value: 2,
+    x: 0,
+    y: 0,
+    r: 15,
+    color: 'white',
+    bgColor: 'red',
     children: [{
       value: 5,
+      x: 0,
+      y: 0,
+      r: 15,
+      color: 'white',
+      bgColor: 'red',
       children: null
     }, {
       value: 6,
+      x: 0,
+      y: 0,
+      r: 15,
+      color: 'white',
+      bgColor: 'blue',
       children: null
     }, {
       value: 7,
+      x: 0,
+      y: 0,
+      r: 15,
+      color: 'white',
+      bgColor: 'red',
       children: null
     }, {
       value: 8,
+      x: 0,
+      y: 0,
+      r: 15,
+      color: 'white',
+      bgColor: 'red',
       children: null
     }]
   }]
 };
 
-function drawTrees(nodes: Array<ITree>, range: {l: number, r: number}, level: number) {
+function drawTrees(nodes: Array<ITree>, range: {l: number, r: number}, level: number, parent?: ITree) {
   const height = 50;
-  const margin = (range.r - range.l) / (nodes.length + 1);
+  const margin = (range.r - range.l) / nodes.length;
+  const offset = margin / 2;
   const svg = d3.select('svg');
-  const radius = 15;
-
-  const g = svg.append('g')
-    .selectAll('circle')
-    .data(nodes)
-    .enter()
-    .append('g');
-  const circles = g.append('circle');
-  circles
-    .attr('cx', function (d: ITree, i: number) {
-      return range.l + (i + 1) * margin;
-    })
-    .attr('cy', function (d: ITree, i: number) {
-      return level * height;
-    })
-    .attr('r', function (d: ITree, i: number) {
-      return radius;
-    })
-    .attr('fill', 'red');
-  const text = g.append('text');
-  text.attr('dx', function (d: ITree, i: number) {
-      return range.l + (i + 1) * margin - 5;
-    })
-    .attr('dy', function (d: ITree, i: number) {
-      return level * height + 5;
-    })
-    .text(function (d: ITree) {
-      return d.value;
-    });
   let nodeNum = 0;
   nodes.forEach((node, i) => {
+    node.x = range.l + offset + i * margin;
+    node.y = level * height;
     if (node.children) {
       nodeNum += node.children.length;
     }
@@ -88,10 +112,74 @@ function drawTrees(nodes: Array<ITree>, range: {l: number, r: number}, level: nu
   nodes.forEach((node, i) => {
     if (node.children) {
       const step = ((range.r - range.l) / nodeNum) * node.children.length;
-      drawTrees(node.children, {l: lastL, r: lastL + step}, level + 1);
+      drawTrees(node.children, {l: lastL, r: lastL + step}, level + 1, node);
       lastL = lastL + step;
     }
   });
+
+  const g = svg.append('g');
+
+  if (parent) {
+    const edgeG = g.append('g');
+    const edges = edgeG
+    .selectAll('line')
+    .data(nodes)
+    .enter()
+    .append('line');
+    edges
+      .attr('x1', parent.x)
+      .attr('y1', parent.y)
+      .attr('x2', function (d: ITree, i: number) {
+        return range.l + offset + i * margin;
+      })
+      .attr('y2', function (d: ITree, i: number) {
+        return level * height;
+      })
+      .style('stroke', function (d: ITree, i: number) {
+        return d.bgColor;
+      })
+      .style('stroke-width', '1')
+      .style('z-index', '-1');
+  }
+
+  const circleG = g.append('g');
+  const circles = circleG
+    .selectAll('circle')
+    .data(nodes)
+    .enter()
+    .append('circle');
+  circles
+    .attr('cx', function (d: ITree, i: number) {
+      return d.x;
+    })
+    .attr('cy', function (d: ITree, i: number) {
+      return d.y;
+    })
+    .attr('r', function (d: ITree, i: number) {
+      return d.r;
+    })
+    .attr('fill', function (d: ITree, i: number) {
+      return d.bgColor;
+    });
+
+  const textG = g.append('g');
+  const text = textG
+    .selectAll('text')
+    .data(nodes)
+    .enter()
+    .append('text');
+  text.attr('dx', function (d: ITree, i: number) {
+      return d.x - 5;
+    })
+    .attr('dy', function (d: ITree, i: number) {
+      return d.y + 5;
+    })
+    .text(function (d: ITree) {
+      return d.value;
+    })
+    .attr('fill', function (d: ITree, i: number) {
+      return d.color;
+    });
 }
 
 function drawChart(data: ITree) {
